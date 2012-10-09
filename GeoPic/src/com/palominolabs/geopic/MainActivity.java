c4fe595da7c0b5e932aa65174de4ac2b18168d31
Manuel Wudka-Robles <mwudka@me.com>
2012-10-08 19:03:47 -0700

    Log location changes

--- GeoPic/src/com/palominolabs/geopic/MainActivity.java
+++ GeoPic/src/com/palominolabs/geopic/MainActivity.java
@@ -1,10 +1,16 @@
 package com.palominolabs.geopic;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.maps.MapActivity;
 
-public class MainActivity extends MapActivity {
+public class MainActivity extends MapActivity implements LocationListener {
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,45 @@ public class MainActivity extends MapActivity {
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
+	}
+
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		LocationManager locationManager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+
+		final int MIN_UPDATE_INTERVAL_MS = 5 * 1000;
+		final int MIN_UPDATE_DISTANCE_METERS = 100;
+
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				MIN_UPDATE_INTERVAL_MS, MIN_UPDATE_DISTANCE_METERS, this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+		LocationManager locationManager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+		locationManager.removeUpdates(this);
+	}
+
+	public void onLocationChanged(Location location) {
+		Log.d("geopic", "Location changed: " + location);
+	}
+
+
+	public void onProviderDisabled(String provider) {
+	}
+
+
+	public void onProviderEnabled(String provider) {
+	}
+
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
 	}
 }
