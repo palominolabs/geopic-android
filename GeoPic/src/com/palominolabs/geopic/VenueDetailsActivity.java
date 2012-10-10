68675ca6951956058b1a8533098734c9f01dbbfb
Manuel Wudka-Robles <mwudka@me.com>
2012-10-09 18:23:57 -0700

    Display venue info in venue details activity

--- GeoPic/src/com/palominolabs/geopic/VenueDetailsActivity.java
+++ GeoPic/src/com/palominolabs/geopic/VenueDetailsActivity.java
@@ -2,6 +2,7 @@ package com.palominolabs.geopic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class VenueDetailsActivity extends Activity {
 
@@ -16,7 +17,18 @@ public class VenueDetailsActivity extends Activity {
         
         setTitle(venue.getName());
         
-        Trace.debug("Showing Venue: " + venue);
+        TextView venueNameView = (TextView) findViewById(R.id.venue_details_nameValue);
+        venueNameView.setText(venue.getName());
+        
+        TextView categoryView = (TextView) findViewById(R.id.venue_details_categoryValue);
+        categoryView.setText(venue.getCategory());
+        
+        TextView checkinsView = (TextView) findViewById(R.id.venue_details_checkinsValue);
+        checkinsView.setText(String.valueOf(venue.getCheckins()));
+        
+        TextView formattedPhoneView = (TextView) findViewById(R.id.venue_details_formattedPhoneValue);
+        formattedPhoneView.setText(venue.getFormattedPhone());
+        
     }
 
 }
