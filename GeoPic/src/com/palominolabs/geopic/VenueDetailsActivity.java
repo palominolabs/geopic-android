9296dfbea8e28d0370d0479ad319c1b626f8d6f2
Manuel Wudka-Robles <mwudka@me.com>
2012-10-09 19:37:24 -0700

    Added button that takes a picture to the venue details activity

--- GeoPic/src/com/palominolabs/geopic/VenueDetailsActivity.java
+++ GeoPic/src/com/palominolabs/geopic/VenueDetailsActivity.java
@@ -1,34 +1,78 @@
 package com.palominolabs.geopic;
 
+import java.io.ByteArrayOutputStream;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class VenueDetailsActivity extends Activity {
 
+	private static final int TAKE_PICTURE_REQUEST_CODE = 1;
+
 	private Venue venue;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_venue_details);
-        
-        venue = (Venue) getIntent().getExtras().getSerializable("venue");
-        
-        setTitle(venue.getName());
-        
-        TextView venueNameView = (TextView) findViewById(R.id.venue_details_nameValue);
-        venueNameView.setText(venue.getName());
-        
-        TextView categoryView = (TextView) findViewById(R.id.venue_details_categoryValue);
-        categoryView.setText(venue.getCategory());
-        
-        TextView checkinsView = (TextView) findViewById(R.id.venue_details_checkinsValue);
-        checkinsView.setText(String.valueOf(venue.getCheckins()));
-        
-        TextView formattedPhoneView = (TextView) findViewById(R.id.venue_details_formattedPhoneValue);
-        formattedPhoneView.setText(venue.getFormattedPhone());
-        
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_venue_details);
+
+		venue = (Venue) getIntent().getExtras().getSerializable("venue");
+
+		setTitle(venue.getName());
+
+		TextView venueNameView = (TextView) findViewById(R.id.venue_details_nameValue);
+		venueNameView.setText(venue.getName());
+
+		TextView categoryView = (TextView) findViewById(R.id.venue_details_categoryValue);
+		categoryView.setText(venue.getCategory());
+
+		TextView checkinsView = (TextView) findViewById(R.id.venue_details_checkinsValue);
+		checkinsView.setText(String.valueOf(venue.getCheckins()));
+
+		TextView formattedPhoneView = (TextView) findViewById(R.id.venue_details_formattedPhoneValue);
+		formattedPhoneView.setText(venue.getFormattedPhone());
+
+		View takePictureButton = (View) findViewById(R.id.venue_details_takePicture);
+		takePictureButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				takePicture();
+			}
+		});
+
+	}
+
+	private void takePicture() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(takePictureIntent, TAKE_PICTURE_REQUEST_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == TAKE_PICTURE_REQUEST_CODE) {
+			handlePictureResult(data);
+		} else {
+			super.onActivityResult(requestCode, resultCode, data);
+		}
+	}
+
+	private void handlePictureResult(Intent data) {
+		if (data == null || data.getExtras() == null || !data.getExtras().containsKey("data")) {
+			return;
+		}
+		Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		bitmap.compress(CompressFormat.JPEG, 100, baos);
+
+		byte[] photoBytes = baos.toByteArray();
+		Trace.debug("Got JPEG with byte size: " + photoBytes.length);
+	}
 
 }
